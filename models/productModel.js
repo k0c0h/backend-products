6b13027c3d9087f90176a10fbbbffe19432891af
Kerlly Alexandra Chiriboga Cabrera <chiribogakerly@gmail.com>
2026-02-09 18:47:23 -0500

    fix errors

--- models/productModel.js
+++ models/productModel.js
@@ -1,12 +1,19 @@
 const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
-const uri = "mongodb+srv://mrsproudd:mrsproudd@cluster0.ad7fs0q.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
 
 async function getAllProducts() {
-  await client.connect();
-  const db = client.db("EXAM3");
-  return db.collection("products").find({}).toArray();
+  try {
+    await client.connect();
+    const db = client.db("EXAM3");
+    const products = await db.collection("products").find({}).toArray();
+    return products;
+  } catch (error) {
+    console.error("Error getting products:", error);
+    throw error;
+  }
 }
 
 module.exports = { getAllProducts };
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const productRoutes = require("./routes/productRoutes");
 
+const PORT = process.env.PORT || 3008;
+
 const server = http.createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
@@ -15,6 +17,6 @@ const server = http.createServer((req, res) => {
   productRoutes(req, res);
 });
 
-server.listen(3008, () => {
-  console.log("Products backend running on http://localhost:3008");
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Products backend running on port ${PORT}`);
 });
